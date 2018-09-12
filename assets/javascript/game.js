155cdf5c31812776e6aff6c6fcae52cea1ece23b
Libby Hughes <libbyhughes@Libbys-MacBook-Pro.local>
2018-09-11 23:15:14 -0600

    stuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,157 +1,96 @@
-var spells;
-spells = ["riddikulus", "obliviate", "sectumsempra", "avada kedavra", "alohomora", "lumos", "expelliarmus", "wingardium leviosa", "accio", "expecto patronum"]
+var spells = ["riddikulus", "obliviate", "sectumsempra", "avada kedavra", "alohomora", "lumos", "expelliarmus", "wingardium leviosa", "accio", "expecto patronum"]
 
-const maxTries = 10;            // Maximum number of tries player has
+var guessesLeft = 10;
 
-var guessedLetters = [];        // Stores the letters the user guessed
-var currentWordIndex;           // Index of the current word in the array
-var guessingWord = [];          // This will be the word we actually build to match the current word
-var remainingGuesses = 0;       // How many tries the player has left
-var gameStarted = false;        // Flag to tell if the game has started
-var hasFinished = false;        // Flag for 'press any key to try again'     
-var wins = 0;                   // How many wins has the player racked up
+var wrongGuess = [];
+var guessingWord = "";
+var blanks = 0;
+var wordLtr = [];
+var winNumber = 0;
+var lossNumber = 1;
+var blanksSuccess = [];
 
-    // Use Math.floor to round the random number down to the nearest whole.
-    currentWordIndex = Math.floor(Math.random() * (selectableWords.length));
+function startGame() {
+    wrongGuess = [];
+    guessesLeft = 10;
+    blanksSuccess = [];
 
-    // Clear out arrays
-    guessedLetters = [];
-    guessingWord = [];
+    guessingWord = spells[Math.floor(Math.random() * wordBank.length)];
+    chosenLtr = guessingWord.split("");
+    Blanks = chosenLtr.length;
+    console.log(guessingWord);
+    console.log(Blanks);
 
-    // Make sure the hangman image is cleared
-    document.getElementById("hangmanImage").src = "";
-
-    // Build the guessing word and clear it out
-    for (var i = 0; i < selectableWords[currentWordIndex].length; i++) {
-        guessingWord.push("_");
+    for (var i = 0; i < Blanks; i++) {
+            blanksSuccess.push("_");
     }
-    // Hide game over and win images/text
-    document.getElementById("pressKeyTryAgain").style.cssText= "display: none";
-    document.getElementById("gameover-image").style.cssText = "display: none";
-    document.getElementById("youwin-image").style.cssText = "display: none";
+    console.log(blanksSuccess);
 
-    // Show display
-    updateDisplay();
-};
+    document.getElementById('word-blank').innerHTML = blanksSuccess.join(" ");
+    document.getElementById('guesses-left').innerHTML = guessesLeft;
 
-//  Updates the display on the HTML Page
-function updateDisplay() {
 
-    document.getElementById("totalWins").innerText = wins;
-    document.getElementById("currentWord").innerText = "";
-    for (var i = 0; i < guessingWord.length; i++) {
-        document.getElementById("currentWord").innerText += guessingWord[i];
-    }
-    document.getElementById("remainingGuesses").innerText = remainingGuesses;
-    document.getElementById("guessedLetters").innerText = guessedLetters;
-    if(remainingGuesses <= 0) {
-        document.getElementById("gameover-image").style.cssText = "display: block";
-        document.getElementById("pressKeyTryAgain").style.cssText = "display:block";
-        hasFinished = true;
-    }
-};
+}
 
-document.onkeydown = function(event) {
-    // If we finished a game, dump one keystroke and reset.
-    if(hasFinished) {
-        resetGame();
-        hasFinished = false;
-    } else {
-        // Check to make sure a-z was pressed.
-        if(event.keyCode >= 65 && event.keyCode <= 90) {
-            makeGuess(event.key.toLowerCase());
-        }
-    }
-};
- 
-function makeGuess(letter) {
-    if (remainingGuesses > 0) {
-        if (!gameStarted) {
-            gameStarted = true;
-        }
-
-        // Make sure we didn't use this letter yet
-        if (guessedLetters.indexOf(letter) === -1) {
-            guessedLetters.push(letter);
-            evaluateGuess(letter);
-        }
-    }
+function checkLetters(letter) {
     
-    updateDisplay();
-    checkWin();
-};
+    var letterInWord = false;
 
-// This function takes a letter and finds all instances of 
-// appearance in the string and replaces them in the guess word.
-function evaluateGuess(letter) {
-    // Array to store positions of letters in string
-    var positions = [];
-
-    // Loop through word finding all instances of guessed letter, store the indicies in an array.
-    for (var i = 0; i < selectableWords[currentWordIndex].length; i++) {
-        if(selectableWords[currentWordIndex][i] === letter) {
-            positions.push(i);
+    for(var i = 0; i < Blanks; i++) {
+        if(guessingWord[i] === letter) {
+            letterInWord = true;
+            
         }
     }
 
-    // if there are no indicies, remove a guess and update the hangman image
-    if (positions.length <= 0) {
-        remainingGuesses--;
-        updateHangmanImage();
-    } else {
-        // Loop through all the indicies and replace the '_' with a letter.
-        for(var i = 0; i < positions.length; i++) {
-            guessingWord[positions[i]] = letter;
+    if(letterInWord) {
+        for(i = 0; i < Blanks; i++) {
+            if(guessingWord[i] === letter) {
+                blanksSuccess[i] = letter;
+            }
         }
     }
-};
-
-// This function takes a letter and finds all instances of 
-// appearance in the string and replaces them in the guess word.
-function evaluateGuess(letter) {
-    // Array to store positions of letters in string
-    var positions = [];
-
-    // Loop through word finding all instances of guessed letter, store the indicies in an array.
-    for (var i = 0; i < selectableWords[currentWordIndex].length; i++) {
-        if(selectableWords[currentWordIndex][i] === letter) {
-            positions.push(i);
-        }
+    else {
+        guessesLeft --;
+        wrongGuess.push(letter);
     }
+  
 
-    // if there are no indicies, remove a guess and update the hangman image
-    if (positions.length <= 0) {
-        remainingGuesses--;
-        updateHangmanImage();
-    } else {
-        // Loop through all the indicies and replace the '_' with a letter.
-        for(var i = 0; i < positions.length; i++) {
-            guessingWord[positions[i]] = letter;
-        }
+}
+
+function finishGame() {
+
+    document.getElementById("word-blank").innerHTML = blanksSuccess.join(" ");
+    document.getElementById("guesses-left").innerHTML = guessesLeft;
+    document.getElementById("wrong-guesses").innerHTML = wrongGuess.join(" ");
+
+
+    if(chosenLtr.join(" ") === blanksSuccess.join(" ")) {
+        winNumber++;
+        alert("Winner takes it all!!!!  Your word is " + guessingWord);
+        document.getElementById("win-counter").innerHTML = winNumber;
+        startGame();
     }
-};
-
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var wins = 0;
-var losses = 0;
-var guesses = "10"
-
-    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-    console.log(computerChoice)
-
-document.onkeypress = function(event) {
-    var userGuess = event.key; 
-    if(userGuess === computer.Choice) {
-        wins++;
-    }
-    else{guesses--;
-    }
-
-    if(guesses === 0) {
-        losses++
+    else if(guessesLeft === 0) {
+        document.getElementById("loss-counter").innerHTML = lossNumber ++;
+        document.getElementById("wrong-guesses").innerHTML = "";
+        alert("I'm not taking a chance on you, " + guessingWord + " try again!");
+        startGame();
     }
 }
 
-document.getElementById("wins").innerHTML = "Wins: " + wins;
-document.getElementById("losses").innerHTML = "losses: " + losses;
-document.getAnimations("guesses").innerHTML = "guesses left: " + guesses;
+startGame();
+
+document.onkeyup = function(event) {
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(letterGuessed)
+    finishGame();
+}
+
+    document.getElementById("starter").onclick(resetGame)
+    wrongGuesses = [];
+    guessesLeft = 10;
+    blanksSuccess = [];
+
+
+
